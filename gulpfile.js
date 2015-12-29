c34f7de40a15032400113d0fe727b9600ba43a36
Braden Snell <braden.snell@byu.edu>
2015-12-29 11:56:36 -0700

    deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,21 @@ var Builder = require('systemjs-builder');
 var less = require('less');
 var fs = require('fs');
 var liveServer = require('live-server');
+var shell = require('shelljs');
 
 //AWESOMER
 gulp.task('build', doLess);
 
 gulp.task('start', function() {
     liveServer.start({port: 8081});
+});
+
+gulp.task('deploy', ['build'], function() {
+    shell.cp('-rf', 'out/Datium.js', 'deploy/Datium.js');
+    shell.cd('deploy');
+    shell.exec('git add -A');
+    shell.exec('git commit -m "latest deploy"');
+    shell.exec('git push');
 });
 
 function doLess() {
